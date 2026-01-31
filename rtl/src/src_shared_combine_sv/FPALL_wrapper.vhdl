b8cf3a817506edcacebffe39004c39104fbb15db
Rikuto Ide <iderikut@bastet.eecg>
2026-01-31 17:24:25 -0500

    enum

--- rtl/src/src_shared_combine_sv/FPALL_wrapper.vhdl
+++ rtl/src/src_shared_combine_sv/FPALL_wrapper.vhdl
@@ -3,7 +3,8 @@ use ieee.std_logic_1164.all;
 
 entity FPALL_Shared_Wrapper is
     generic (
-        OP_CODE_GEN : std_logic_vector(1 downto 0) := "00" -- 00: Add, 01: Mul, 10: Sqrt, 11: Div
+        OP_CODE_GEN : std_logic_vector(1 downto 0) := "00"; -- 00: Add, 01: Mul, 10: Sqrt, 11: Div
+        FMT_GEN     : std_logic := '0'                      -- '0': FP32, '1': FP16
     );
     port (
         clk : in std_logic;
@@ -18,6 +19,7 @@ architecture arch of FPALL_Shared_Wrapper is
         port (
             clk : in std_logic;
             opcode : in std_logic_vector(1 downto 0);
+            fmt : in std_logic;
             X : in std_logic_vector(31 downto 0);
             Y : in std_logic_vector(31 downto 0);
             R : out std_logic_vector(31 downto 0)
@@ -28,6 +30,7 @@ begin
     port map (
         clk => clk,
         opcode => OP_CODE_GEN,
+        fmt => FMT_GEN,
         X => X(31 downto 0),
         Y => Y(31 downto 0),
         R => R(31 downto 0)
